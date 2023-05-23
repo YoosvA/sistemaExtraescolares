44e18abf249b3cc96a8ce5ccaa623bdd7dd96463
YoosvA <Computadoraz63@gmail.com>
2023-05-23 11:35:23 -0600

    datosmigrate

---
+++ database/migrations/2023_05_23_172058_create_datos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDatosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('datos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellidoPaterno');
+            $table->string('apellidoMaterno');
+            $table->string('noControl');
+            $table->string('telefono');
+            $table->string('carrera');
+            $table->date('fechaNac');
+            $table->string('escuelaProcedencia');
+            $table->date('fechaIngreso');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('datos');
+    }
+}
