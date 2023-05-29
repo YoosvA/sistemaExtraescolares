96b550790eaa92148357c0c85343e73abbdcef21
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-05-29 13:47:11 -0600

    NuevaInformacion

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,129 +12,179 @@
 </head>
 
 <body class="colorDeFondo">
-    <section class="sidebar">
-        <!---->
-        <br>
-        <div class="nav-header">
-            <p class="logo">Sistema ExtraEscolares</p>
-            <i class="bx bx-menu btn-menu"></i>
-        </div>
-        <!---->
-        <ul class="nav-links">
-            <li>
-               <div class="text-center">
-                <img src="{{ asset('img/LOGO_TECNM_BLANCO.png') }}" class="title tamañoDeImgLogo" alt="">
-                <span class="tooltip"></span>
-               </div>
-            </li>
-            <li>
-                <a href="#DatosPersonales">
-
-                    <span class="title">Datos Personales <br> Del Alumno</span>
-                </a>
-                <span class="tooltip">Datos Personales Del Alumno</span>
-            </li>
-            <li>
-                <a href="#NotasDeParticipacion">
-                    <span class="title">Notas De <br> Participacion ( Eventos )</span>
-                </a>
-                <span class="tooltip">Notas De <br> Participacion ( Eventos )</span>
-            </li>
-            <li>
-                <a href="#Creditos">
-                    <span class="title">Creditos</span>
-                </a>
-                <span class="tooltip">Creditos</span>
-            </li>
-            <li>
-                <a href="/agregarUsuario">
-                    <span class="title">Agregar Usuario</span>
-                </a>
-                <span class="tooltip">Agregar Usuario</span>
-            </li>
-            <li>
-                <a href="{{ route('logout') }}">
-                    <span class="title">Cerrar Seccion</span>
-                </a>
-                <span class="tooltip">Cerrar Seccion</span>
-            </li>
-        </ul>
-        <div class="theme-wrapper">
-            <i class="bx bxs-moon theme-icon"></i>
-            <p>Dark Theme</p>
-            <div class="theme-btn">
-                <span class="theme-ball"></span>
+    <main class="main">
+        <section class="menu" id="menu">
+            <button>=</button>
+            <div class="menu-content">
+                <ul>
+                    <li>
+                        <div class="text-center"> &nbsp;&nbsp;&nbsp; <img src="{{ asset('img/LOGO_TECNM_BLANCO.png') }}"
+                                class="tamañoDeLogoBlanco img-fluid" alt=""></div>
+                    </li>
+                    <li>
+                        <h3 class="text-center text-white">Sistema <br> ExtraEscolares</h3>
+                    </li>
+                    <li><a href="#DatosPersonalesDelAlumno">Datos Personales <br> Del Alumno</a></li>
+                    <li><a href="#Creditos">Creditos</a></li>
+                    <li><a href="#NotasDeParticipacion">Notas De <br> Participacion</a></li>
+                    <li><a href="/agregarUsuario">Agregar Usuario</a></li>
+                    <li><a href="{{ route('logout') }}">Cerrar Seccion</a></li>
+                    <li class="colorBlanco">
+                        <hr>
+                    </li>
+                    <li class="colorBlanco text-center">Creative Code</li>
+                </ul>
             </div>
-        </div>
-    </section>
-
-
-    <section class="home">
-        <h2 id="DatosPersonales">DatosPersonales</h2>
-        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum exercitationem dolorem sunt reprehenderit
-            mollitia qui corrupti molestias ex vel porro quos, rerum esse ducimus. Ipsam provident dignissimos quia
-            distinctio nam.</h4>
-
-        <a href="/vistaAlumnos" class="btn btn-primary">Vista Datos</a>
-
-        <h2 id="NotasDeParticipacion">NotasDeParticipacion</h2>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quos sequi expedita quo odit inventore
-            neque quisquam praesentium! Eveniet aut mollitia quo vel commodi delectus, natus autem ipsa recusandae
-            voluptas!.
-        </h1>
-
-        <h2 id="Creditos">Creditos</h2>
-        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quos sequi expedita quo odit inventore
-            neque quisquam praesentium! Eveniet aut mollitia quo vel commodi delectus, natus autem ipsa recusandae
-            voluptas!
-            <a href="/vistaCreditos" class="btn btn-primary">Creditos</a>
-        </h1>
-    </section>
-
-
+        </section>
+        <section class="content">
+            <!---->
+            <br><br>
+            <h1 id="DatosPersonalesDelAlumno">Datos Personales Del Alumno</h1>
+            <h2 class="ms-5">Caracteristicas</h2>
+            <h4 class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt, id corporis.
+                Eveniet praesentium assumenda laudantium deserunt omnis est maxime, rerum rem aliquid
+                vel tempore incidunt perferendis eos quasi nostrum odit!Lorem ipsum dolor sit amet consectetur
+                adipisicing elit. Deserunt, id corporis.
+                Eveniet praesentium assumenda laudantium deserunt omnis est maxime, rerum rem aliquid
+                vel tempore incidunt perferendis eos quasi nostrum odit!</h4>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card border-0 colorTransparente">
+                        <div class="card-body">
+                            <h5 class="mt-4 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+                                id corporis.
+                                Eveniet praesentium assumenda laudantium deserunt omnis est maxime, rerum rem aliquid
+                                vel tempore incidunt perferendis eos quasi nostrum odit!Lorem ipsum dolor sit amet
+                                consectetur adipisicing elit. Deserunt, id corporis.
+                                Eveniet praesentium assumenda laudantium deserunt omnis est maxime, rerum rem aliquid
+                                vel tempore incidunt perferendis eos quasi nostrum odit!</h5>
+                            <!--Parte de boton-->
+                            <br>
+                            <div class="d-grid gap-2 col-4 mt-4 mx-auto">
+                                <a href="/vistaAlumnos" class="btn colorDeBotonazul rounded-pill border-0">
+                                    <h5 class="text-white">Vista Datos</h5>
+                                </a>
+                            </div>
+                            <!--Parte de boton-->
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card border-0 colorTransparente">
+                        <div class="card-body">
+                            <div class="text-center">
+                                <img src="{{ asset('img/Persona1.jpg') }}"
+                                    class="rounded-circle tamañoDeImgPersona img-fluid" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <br><br><br><br>
+            <!---->
+            <h1 id="Creditos">Creditos</h1>
+            <h2 class="ms-5">Caracteristicas</h2>
+            <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quos sequi expedita quo odit
+                inventore
+                neque quisquam praesentium! Eveniet aut mollitia quo vel commodi delectus, natus autem ipsa recusandae
+                voluptas!Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quos sequi expedita quo
+                odit
+                inventore
+                neque quisquam praesentium! Eveniet aut mollitia quo vel commodi delectus, natus autem ipsa recusandae
+                voluptas!</h5>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card border-0 colorTransparente">
+                        <div class="card-body">
+                            <div class="text-center">
+                                <img src="{{ asset('img/Persona2.jpg') }}"
+                                    class="rounded-circle tamañoDeImgPersona img-fluid" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="card border-0 colorTransparente">
+                        <div class="card-body">
+                            <h5 class="mt-4 mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt,
+                                id corporis.
+                                Eveniet praesentium assumenda laudantium deserunt omnis est maxime, rerum rem aliquid
+                                vel tempore incidunt perferendis eos quasi nostrum odit!Lorem ipsum dolor sit amet
+                                consectetur adipisicing elit. Deserunt, id corporis.
+                                Eveniet praesentium assumenda laudantium deserunt omnis est maxime, rerum rem aliquid
+                                vel tempore incidunt perferendis eos quasi nostrum odit!</h5>
+                            <!--Parte de boton-->
+                            <br>
+                            <div class="d-grid gap-2 col-4 mt-4 mx-auto">
+                                <a href="/vistaCreditos" class="btn colorDeBotonazul rounded-pill border-0">
+                                    <h5 class="text-white">Creditos</h5>
+                                </a>
+                            </div>
+                            <!--Parte de boton-->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!---->
+            <h2 id="NotasDeParticipacion">Notas De <br> Participacion</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reiciendis officia similique
+                blanditiis eaque
+                laboriosam a fugiat, excepturi nemo quod asperiores. Itaque sed unde vitae modi velit, minima iste
+                odit corporis
+                facere similique dolore laudantium amet, eaque, ratione voluptatem. Autem dolorem accusamus
+                quibusdam, vero
+                dignissimos blanditiis. Placeat, modi doloremque similique expedita voluptatibus rerum.
+                Reprehenderit rem eveniet
+                facilis modi facere, explicabo dolorem in perspiciatis distinctio magni deserunt excepturi
+                doloremque! Ex
+                accusantium
+                rerum ullam magni earum? Sed tenetur corrupti ducimus voluptatum nesciunt consequuntur
+                exercitationem necessitatibus
+                quod itaque dolorum beatae maxime maiores, cum suscipit quam deserunt eaque temporibus facilis
+                cupiditate fugiat
+                voluptate quis.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reiciendis officia similique
+                blanditiis eaque
+                laboriosam a fugiat, excepturi nemo quod asperiores. Itaque sed unde vitae modi velit, minima iste
+                odit corporis
+                facere similique dolore laudantium amet, eaque, ratione voluptatem. Autem dolorem accusamus
+                quibusdam, vero
+                dignissimos blanditiis. Placeat, modi doloremque similique expedita voluptatibus rerum.
+                Reprehenderit rem eveniet
+                facilis modi facere, explicabo dolorem in perspiciatis distinctio magni deserunt excepturi
+                doloremque! Ex
+                accusantium
+                rerum ullam magni earum? Sed tenetur corrupti ducimus voluptatum nesciunt consequuntur
+                exercitationem necessitatibus
+                quod itaque dolorum beatae maxime maiores, cum suscipit quam deserunt eaque temporibus facilis
+                cupiditate fugiat
+                voluptate quis.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum reiciendis officia similique
+                blanditiis eaque
+                laboriosam a fugiat, excepturi nemo quod asperiores. Itaque sed unde vitae modi velit, minima iste
+                odit corporis
+                facere similique dolore laudantium amet, eaque, ratione voluptatem. Autem dolorem accusamus
+                quibusdam, vero
+                dignissimos blanditiis. Placeat, modi doloremque similique expedita voluptatibus rerum.
+                Reprehenderit rem eveniet
+                facilis modi facere, explicabo dolorem in perspiciatis distinctio magni deserunt excepturi
+                doloremque! Ex
+                accusantium
+                rerum ullam magni earum? Sed tenetur corrupti ducimus voluptatum nesciunt consequuntur
+                exercitationem necessitatibus
+                quod itaque dolorum beatae maxime maiores, cum suscipit quam deserunt eaque temporibus facilis
+                cupiditate fugiat
+                voluptate quis.
+            </p>
+            <!---->
+        </section>
+    </main>
     <script>
-        const btn_menu = document.querySelector(".btn-menu");
-        const side_bar = document.querySelector(".sidebar");
-
-        btn_menu.addEventListener("click", function() {
-            side_bar.classList.toggle("expand");
-            changebtn();
-        });
-
-        function changebtn() {
-            if (side_bar.classList.contains("expand")) {
-                btn_menu.classList.replace("bx-menu", "bx-menu-alt-right");
-            } else {
-                btn_menu.classList.replace("bx-menu-alt-right", "bx-menu");
-            }
-        }
-
-        const btn_theme = document.querySelector(".theme-btn");
-        const theme_ball = document.querySelector(".theme-ball");
-
-        const localData = localStorage.getItem("theme");
-
-        if (localData == null) {
-            localStorage.setItem("theme", "light");
-        }
-
-        if (localData == "dark") {
-            document.body.classList.add("dark-mode");
-            theme_ball.classList.add("dark");
-        } else if (localData == "light") {
-            document.body.classList.remove("dark-mode");
-            theme_ball.classList.remove("dark");
-        }
-
-        btn_theme.addEventListener("click", function() {
-            document.body.classList.toggle("dark-mode");
-            theme_ball.classList.toggle("dark");
-            if (document.body.classList.contains("dark-mode")) {
-                localStorage.setItem("theme", "dark");
-            } else {
-                localStorage.setItem("theme", "light");
-            }
-        });
+        const menu = document.getElementById('menu')
+        menu.addEventListener('click', () => {
+            menu.classList.toggle('menu-activo');
+        })
     </script>
 </body>
 
