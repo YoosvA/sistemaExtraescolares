9f532072a511f61704779c38d34823f0aec91959
YoosvA <Computadoraz63@gmail.com>
2023-05-31 21:08:00 -0600

    rol

--- resources/views/notas/generarPDF.blade.php
+++ resources/views/notas/generarPDF.blade.php
@@ -36,35 +36,121 @@
 
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>Nota de Participacion</title>
 </head>
+
 <body>
 
 
-    <div class="conatiner">
+    <div class="conatiner mt-4">
         <div class="row">
-            <div class="col">
-                <div class="alert alert-primary" role="alert">
-                    <p><strong>Nombre:</strong> {{ $nota->nombreAlumnoNotas }}</p>
-                    <p><strong>Apellido Paterno:</strong> {{ $nota->apellidoPaternoNotas }}</p>
-                    <p><strong>Apellido Materno:</strong> {{ $nota->apellidoMaternoNotas }}</p>
-                    <p><strong>Número de Control:</strong> {{ $nota->noControlNotas }}</p>
-                    <p><strong>Carrera:</strong> {{ $nota->carreraNotas }}</p>
-                    <p><strong>Evento:</strong> {{ $nota->evento }}</p>
-                    <p><strong>Horas de Participación:</strong> {{ $nota->horas }} Horas</p>
-                    <p><strong>Fecha:</strong> {{ $nota->fecha }}</p>
-                    <p><strong>Periodo Escolar:</strong> {{ $nota->periodo }}</p>
-                    <p><strong>Grupo:</strong> {{ $nota->grupo }}</p>
+            <div class="col text-center">
+                <div>
+                    <h3>
+                        <b>INSTITUTO TECNOLÓGICO DE MILPA ALTA ll
+                        </b>
+                    </h3>
+                </div>
+            </div>
+            <div class="col text-center">
+                <div>
+                    <h3>
+                        <b>DEPARTAMENTO DE ACTIVIDADES <br> EXTRAESCOLARES
+                    </b>
+                </h3>
                 </div>
             </div>
         </div>
     </div>
 
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <table class="table border border-dark text-center">
+                    <thead>
+                        <tr>
+                            <th class="border border-dark">Numero de Control:</th>
+                            <th class="border border-dark">Periodo Escolar:</th>
+                            <th class="border border-dark">Nombre:</th>           
+                        </tr>
+                    </thead>
+                    <thead>
+                        <tr>
+                            <td class="border border-dark">{{ $nota->noControlNotas }}</td>
+                            <td class="border border-dark">{{ $nota->periodo }}</td>
+                            <td class="border border-dark">{{ $nota->apellidoPaternoNotas }} {{ $nota->apellidoMaternoNotas }} {{ $nota->nombreAlumnoNotas }}</td>
+                            
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="col">
+                <table class="table border border-dark text-center">
+                    <thead>
+                        <tr>
+                            <th class="border border-dark">Actividad:</th>
+                            <th class="border border-dark">Fecha:</th>
+                            <th class="border border-dark">Grupo:</th>
+                            <th class="border border-dark">Hrs. Acreditadas</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="border border-dark">{{ $nota->evento }}</td>
+                            <td class="border border-dark">{{ $nota->fecha }}</td>
+                            <td class="border border-dark">{{ $nota->grupo }}</td>
+                            <td class="border border-dark">{{ $nota->horas }} Horas</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="col">
+                <table class="table border border-dark text-center">
+                    <thead>
+                        <tr>
+                            <th class="border border-dark">Carrera:</th>
+                            <td class="border border-dark">{{ $nota->carreraNotas }}</td>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <small><b>NOTA: </b>EL ALUMNO DEBE DE CONSERVAR ESTE DOCUMENTO Y ENTREGARLO AL DEPARTAMENTO DE
+                    EXTRAESCOLARES AL COMPLETAR UN CREDITO EQUIVALENTE A 20 HRS.
+                    <br><br>
+                    ESTE DOCUMENTO SERÁ JUSTIFICACIÓN PARA SUS ACTIVIDADES EXTRAESCOLARES EN CADA UNA DE SUS
+                    ASIGNATURAS.
+                    <br><br>
+                    PARA QUE TENGA VALIDEZ ESTE DOCUMENTO DEBE TENER FIRMA Y SELLO DEL JEFE DE DEPARTAMENTO
+                    QUE PROPORCIONO EL APOYO.
+                </small>
+            </div>
+        </div>
+        <div class="row mt-4 text-center">
+            <small><b>ING. ROLDAN AQUINO SEGURA</b>
+                <br>JEFE DEL DEPARTAMENTO DE ACTIVIDADES EXTRAESCOLARES
+                <br><br>
+                _____________________
+            </small>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
