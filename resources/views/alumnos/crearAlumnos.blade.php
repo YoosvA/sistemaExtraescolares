3f8315da7f579e10759489ce817c16b238e8e778
LuisAlbertoAttack1 <laaolivares837@gmail.com>
2023-06-01 19:53:03 -0600

    QueHicicsteOsvaldo

--- resources/views/alumnos/crearAlumnos.blade.php
+++ resources/views/alumnos/crearAlumnos.blade.php
@@ -1,48 +1,99 @@
 @extends('layouts/main')
 
 @section('contenido')
-    <div class="container">
-        <h4 class="text-center mt-4">Agregar Datos Alumno</h4>
+    <!--Inicio De Container-->
+    <div class="container mt-4">
         <div class="row">
             <div class="col">
-                <form action="/store" method="post">
-                    @csrf
-                    @method('POST')
-                    <div class="row">
-                        <div class="col">
-                            <label for="nombre">Nombre del Alumno</label>
-                            <input type="text" name="nombre" id="nombre" class="form-control" required>
-                            <label for="apellidoPaterno">Apellido Paterno</label>
-                            <input type="text" name="apellidoPaterno" id="apellidoPaterno" class="form-control" required>
-                            <label for="apellidoMaterno">Apellido Materno</label>
-                            <input type="text" name="apellidoMaterno" id="apellidoMaterno" class="form-control" required>
-                            <label for="noControl">Numero de Control</label>
-                            <input type="text" name="noControl" id="noControl" class="form-control" required>
-                            <label for="telefono">Numero de Celular</label>
-                            <input type="text" name="telefono" id="telefono" class="form-control" required>
+                <!--Inicio De Contenido-->
+                <h1 class="mt-4">Agregar Datos Alumno</h1>
+                <h2 class="ms-4">Instrucciones</h2>
+                <h5>Ingresa todos los datos que se te piden</h5>
+                <!--Inicio de Card-->
+                <div class="card mt-4 shadow-lg bg-body rounded border-0">
+                    <div class="card-body">
+                        <!--Contenido Dentro De Card Inicio-->
+                        <div class="row">
+                            <div class="col">
+                                <form action="/store" method="post">
+                                    @csrf
+                                    @method('POST')
+                                    <div class="row">
+                                        <div class="col">
+                                            <h4 class="mt-3">Nombre Del Alumno</h4>
+                                            <input type="text" name="nombre" id="nombre" class="form-control rounded-pill text-center"
+                                                required placeholder="Nombre Del Alumno" aria-label="Nombre Del Alumno">
+                                            <h4 class="mt-3">Apellido Paterno</h4>
+                                            <input type="text" name="apellidoPaterno" id="apellidoPaterno"
+                                                class="form-control rounded-pill text-center" required placeholder="Apellido Paterno" aria-label="Apellido Paterno">
+                                            <h4 class="mt-3">Apellido Materno</h4>
+                                            <input type="text" name="apellidoMaterno" id="apellidoMaterno"
+                                                class="form-control rounded-pill text-center" required placeholder="Apellido Materno" aria-label="Apellido Materno">
+                                            <h4 class="mt-3">Numero De Control</h4>
+                                            <input type="text" name="noControl" id="noControl" class="form-control rounded-pill text-center"
+                                                required placeholder="Numero De Control" aria-label="Numero De Control">
+                                            <h4 class="mt-3">Numero De Celular</h4>
+                                            <input type="text" name="telefono" id="telefono" class="form-control rounded-pill text-center"
+                                                required placeholder="Numero De Celular" aria-label="Numero De Celular">
+                                        </div>
+                                        <div class="col">
+                                            <h4 class="mt-3">Carrera</h4>
+                                            <select name="carrera" id="carrera" class="form-select rounded-pill text-center" required>
+                                                <option value="" selected>Selecciona una opcion</option>
+                                                @foreach ($carreras as $carrera)
+                                                    <option value="{{ $carrera->nombre }}">{{ $carrera->nombre }}</option>
+                                                @endforeach
+                                            </select>
+                                            <h4 class="mt-3">Fecha de Nacimiento</h4>
+                                            <input type="date" name="fechaNac" id="fechaNac" class="form-control rounded-pill text-center"
+                                                required>
+                                            <h4 class="mt-3">Escuela De Procedencia</h4>
+                                            <input type="text" name="escuelaProcedencia" id="escuelaProcedencia"
+                                                class="form-control text-center rounded-pill" required placeholder="Escuela De Procedencia" aria-label="Escuela De Procedencia">
+                                            <h4 class="mt-3">Fecha De Ingreso Al Tecnologico</h4>
+                                            <input type="date" name="fechaIngreso" id="fechaIngreso" class="form-control text-center rounded-pill"
+                                                required>
+                                            <!--Card Inicio De Botones-->
+                                            <div class="row mt-4">
+                                                <div class="col-sm-6">
+                                                    <div class="card border-0">
+                                                        <div class="card-body">
+                                                            <!---->
+                                                            <div class="d-grid gap-2">
+                                                                <button
+                                                                    class="btn colorDeBoton rounded-pill mt-3 text-white">
+                                                                    Guardar</button>
+                                                            </div>
+                                                            <!---->
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <div class="card border-0">
+                                                        <div class="card-body">
+                                                            <!---->
+                                                            <div class="d-grid gap-2">
+                                                                <a href="/vistaAlumnos"
+                                                                    class="btn btn-danger rounded-pill mt-3"> regresar</a>
+                                                            </div>
+                                                            <!---->
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <!--Card Fin De Botones-->
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
-                        <div class="col">
-                            <label for="carrera">Carrera</label>
-                            <select name="carrera" id="carrera" class="form-select" required>
-                                <option value="" selected>Selecciona una opcion</option>
-                                @foreach ($carreras as $carrera)
-                                    <option value="{{ $carrera->nombre }}">{{ $carrera->nombre }}</option>
-                                @endforeach
-                            </select>
-                            <label for="fechaNac">Fecha de Nacimiento</label>
-                            <input type="date" name="fechaNac" id="fechaNac" class="form-control" required>
-                            <label for="escuelaProcedencia">Escuela de Procedencia</label>
-                            <input type="text" name="escuelaProcedencia" id="escuelaProcedencia" class="form-control"
-                                required>
-                            <label for="fechafechaIngresoNac">Fecha de Ingreso al Tecnologico</label>
-                            <input type="date" name="fechaIngreso" id="fechaIngreso" class="form-control" required>
-                        </div>
+                        <!--Contenido Dentro De Card Fin-->
                     </div>
-
-                    <button class="btn btn-primary mt-3"> Guardar</button>
-                    <a href="/vistaAlumnos" class="btn btn-info mt-3"> regresar</a>
-                </form>
+                </div>
+                <!--Fin de Card-->
+                <!--Fin De Contenido-->
             </div>
         </div>
     </div>
+    <!--Fin De Container-->
 @endsection
--- resources/views/alumnos/vistaAlumnos.blade.php
+++ resources/views/alumnos/vistaAlumnos.blade.php
@@ -1,99 +1,115 @@
 @extends('layouts/main')
 
 @section('contenido')
+<!--Inicio De Container-->
     <div class="container mt-4">
         <div class="row">
             <div class="col">
                 <!--Contenido Inicio-->
                 <h1 class="mt-4">Agregar Datos Alumno</h1>
-                    <h2 class="ms-4">Intrucciones</h2>
-                    <h5>En estos 2 botones tendras opciones donde contendra una de agregar alumno <br>
+                <h2 class="ms-4">Intrucciones</h2>
+                <h5>En estos 2 botones tendras opciones donde contendra una de agregar alumno <br>
                     donde tienes que meter informacion para agregar un nuevo alumno. <br>
                     el boton de regresar te mostraran el mismo menú que puedes ocupar</h5>
-                    <!--Parte De Botones Inicio-->
-<div class="card border-0" style="width: 40rem;">
-<div class="card-body">
-    <!--Contenido De Card Botones Dentro Inicio-->
-    <div class="row">
-        <div class="col-sm-6">
-          <div class="card border-0 text-center">
-            <div class="card-body">
-           <!--Contenido De Boton Agregar Alumno Inicio-->
-           <a href="/crearAlumnos" class="btn btn-primary mt-3">Agregar Alumno</a>
-           <!--Contenido De Boton Agregar Alumno Fin-->
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="card border-0 text-center">
-            <div class="card-body">
-          <!--Contenido de Boton Regresar Inicio-->
-          <a href="/inicio" class="btn btn-danger mt-3 ms-2">Regresar</a>
-          <!--Contenido de Boton Regresar fin-->
-            </div>
-          </div>
-        </div>
-      </div>
-          <!--Contenido De Card Botones Fin-->
-</div>
-</div>
-                    <!--Parte De Botones Fin-->
-                <div class="row">
-                    <div class="col mt-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <table class="display responsive nowrap mt-4" style="width:100%" id="table">
-                                    <thead class="text-center">
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Nombre</th>
-                                            <th>Apellido Paterno</th>
-                                            <th>Apellido Materno</th>
-                                            <th>Numero de Control</th>
-                                            <th>Telefono Celular</th>
-                                            <th>Carrera</th>
-                                            <th>Fecha de Nacimiento</th>
-                                            <th>Escuela de Procedencia</th>
-                                            <th>Fecha de Ingreso al Tecnologico</th>
-                                            <th>Editar</th>
-                                            <th>Eliminar</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($items as $item)
-                                            <tr>
-                                                <td>{{ $item->id }}</td>
-                                                <td>{{ $item->nombre }}</td>
-                                                <td>{{ $item->apellidoPaterno }}</td>
-                                                <td>{{ $item->apellidoMaterno }}</td>
-                                                <td>{{ $item->noControl }}</td>
-                                                <td>{{ $item->telefono }}</td>
-                                                <td>{{ $item->carrera }}</td>
-                                                <td>{{ $item->fechaNac }}</td>
-                                                <td>{{ $item->escuelaProcedencia }}</td>
-                                                <td>{{ $item->fechaIngreso }}</td>
-                                                <td>
-                                                    <br>
-                                                    <a href="{{ route('editarAlumnos', $item->id) }}"
-                                                        class="btn btn-warning">Editar</a>
-                                                </td>
-                                                <td>
-                                                    <form action="{{ route('destroy', $item->id) }}" method="POST">
-                                                        @csrf
-                                                        @method('DELETE')
-                                                        <button class="btn btn-danger">Eliminar</button>
-                                                    </form>
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
+                <!--Parte De Botones Inicio-->
+                <div class="card border-0" style="width: 40rem;">
+                    <div class="card-body">
+                        <!--Contenido De Card Botones Dentro Inicio-->
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <div class="card border-0 text-center">
+                                    <div class="card-body">
+                                        <!--Contenido De Boton Agregar Alumno Inicio-->
+                                        <div class="d-grid gap-2">
+                                            <a href="/crearAlumnos" class="btn colorDeBoton mt-3 rounded-pill text-white">Agregar Alumno</a>
+                                          </div>
+
+                                        <!--Contenido De Boton Agregar Alumno Fin-->
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="card border-0 text-center">
+                                    <div class="card-body">
+                                        <!--Contenido de Boton Regresar Inicio-->
+                                        <div class="d-grid gap-2">
+                                            <a href="/inicio" class="btn btn-danger mt-3 rounded-pill">Regresar</a>
+                                        </div>
+
+                                        <!--Contenido de Boton Regresar fin-->
+                                    </div>
+                                </div>
                             </div>
                         </div>
+                        <!--Contenido De Card Botones Fin-->
                     </div>
                 </div>
-                <!--Contenido Inicio-->
+                <!--Parte De Botones Fin-->
+                <!--Contenido Card Inicio-->   
+                <div class="card shadow-lg bg-body rounded border-0 rounded-5">
+                    <div class="card-body">
+                        <!--Contenido De Informacion Card Inicio-->
+                        <div class="row">
+                            <div class="col mt-4 mb-4 ms-4 mx-4">
+                                <div class="card">
+                                    <div class="card-body">
+                                        <table class="display responsive nowrap" style="width:100%" id="table">
+                                            <thead class="text-center">
+                                                <tr>
+                                                    <th>No.</th>
+                                                    <th>Nombre</th>
+                                                    <th>Apellido Paterno</th>
+                                                    <th>Apellido Materno</th>
+                                                    <th>Numero de Control</th>
+                                                    <th>Telefono Celular</th>
+                                                    <th>Carrera</th>
+                                                    <th>Fecha de Nacimiento</th>
+                                                    <th>Escuela de Procedencia</th>
+                                                    <th>Fecha de Ingreso al Tecnologico</th>
+                                                    <th>Editar</th>
+                                                    <th>Eliminar</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($items as $item)
+                                                    <tr>
+                                                        <td>{{ $item->id }}</td>
+                                                        <td>{{ $item->nombre }}</td>
+                                                        <td>{{ $item->apellidoPaterno }}</td>
+                                                        <td>{{ $item->apellidoMaterno }}</td>
+                                                        <td>{{ $item->noControl }}</td>
+                                                        <td>{{ $item->telefono }}</td>
+                                                        <td>{{ $item->carrera }}</td>
+                                                        <td>{{ $item->fechaNac }}</td>
+                                                        <td>{{ $item->escuelaProcedencia }}</td>
+                                                        <td>{{ $item->fechaIngreso }}</td>
+                                                        <td>
+                                                            <br>
+                                                            <a href="{{ route('editarAlumnos', $item->id) }}"
+                                                                class="btn btn-warning">Editar</a>
+                                                        </td>
+                                                        <td>
+                                                            <form action="{{ route('destroy', $item->id) }}"
+                                                                method="POST">
+                                                                @csrf
+                                                                @method('DELETE')
+                                                                <button class="btn btn-danger">Eliminar</button>
+                                                            </form>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Contenido De Informacion Card Fin-->
+                    </div>
+                </div>
+                <!--Contenido Card Fin-->
             </div>
         </div>
     </div>
+<!--Fin De Card-->
 @endsection
